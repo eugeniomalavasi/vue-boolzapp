4dd29f5c1f6295e7b54b05dafc818fc9a3481d42
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-04-24 09:36:11 +0200

    feat: time updates when a message is sent

--- js/script.js
+++ js/script.js
@@ -190,7 +190,6 @@ createApp({
                 this.messageInput = "";
                 this.controll = 0;
             }
-            console.log(this.controll);
         }, autoAnswer: function () {
             const answArray = ["Mi chiamo Massimo Decimo Meridio, comandante dell'esercito del Nord, generale delle legioni Felix, servo leale dell'unico vero imperatore Marco Aurelio. Padre di un figlio assassinato, marito di una moglie uccisa... e avrò la mia vendetta... in questa vita o nell'altra", 
             "Tutto ebbe inizio con la forgiatura dei grandi Anelli. Tre furono dati agli elfi, gli esseri immortali più saggi e leali di tutti. Sette ai re dei nani, grandi minatori e costruttori di città nelle montagne. E nove, nove Anelli furono dati alla razza degli uomini che più di qualunque cosa desiderano il potere.", 
@@ -228,6 +227,6 @@ createApp({
             this.contacts.forEach(curName => {
                 curName.visible = true
             })
-        },
+        }
     },
 }).mount('#app')
